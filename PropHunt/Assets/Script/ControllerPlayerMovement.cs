7e5e97ada50644807f3be20c930c00872fee713a
eduexile <eduexile@gmail.com>
2020-04-20 18:42:12 +0200

    PlayerWithRigidbody

---
+++ PropHunt/Assets/Script/ControllerPlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerPlayerMovement : MonoBehaviour
+
+    
+{
+    [SerializeField]
+    private float speedPlayer = 5f;
+    private PlayerMotorController motor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        motor = GetComponent<PlayerMotorController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float xMov = Input.GetAxisRaw("Horizontal");
+        float zMov = Input.GetAxisRaw("Vertical");
+
+        Vector3 movHorizontal = transform.right * xMov;
+        Vector3 movVertical = transform.forward * zMov;
+
+        //final movement vector
+        Vector3 velocity = (movHorizontal + movVertical).normalized * speedPlayer;
+
+        //apply movement
+        motor.Move(velocity);
+    }
+}
---
+++ PropHunt/Assets/Script/PlayerMotorController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotorController : MonoBehaviour
+{
+    private Vector3 velocity = Vector3.zero;
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MakeItMove();
+    }
+
+    //Gets movement vector
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    //Performs move based on velocity
+    void MakeItMove()
+    {
+        if(velocity != Vector3.zero)
+        {
+            rb.MovePosition(rb.position + velocity * Time.deltaTime);
+        }
+    }
+}
