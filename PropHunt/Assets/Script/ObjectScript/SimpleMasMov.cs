62a0f34771997b64b9bce098054a1c2cf9660fc8
Kendo777 <36260994+Kendo777@users.noreply.github.com>
2020-06-13 23:58:45 +0200

    Prop

--- PropHunt/Assets/Script/ObjectScript/SimpleMasMov.cs
+++ PropHunt/Assets/Script/ObjectScript/SimpleMasMov.cs
@@ -5,11 +5,40 @@ using UnityEngine;
 public class SimpleMasMov : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float maxSpeed = 20.0f;
-    public float acceleration = 20.0f;
+
     public float speed = 20.0f;
     public float jumpSpeed = 8.0f;
+
+    public float armarioMass;
+    public float barril1Mass;
+    public float barril2Mass;
+    public float basuraMass;
+    public float bombonaMass;
+    public float botella1Mass;
+    public float botella2Mass;
+    public float cajaMass;
+    public float camaMass;
+    public float colchonMass;
+    public float conoMass;
+    public float estanteriaMass;
+    public float jarraMass;
+    public float ladrilloMass;
+    public float lataArrugadaMass;
+    public float lataEnteraMass;
+    public float libroMass;
+    public float lamparaMass;
+    public float macetaMass;
+    public float mesaMass;
+    public float mesillaMass;
+    public float miniArmarioMass;
     public float fridgeMass;
+    public float noodlesMass;
+    public float paleMass;
+    public float sillaMass;
+    public float sillonMass;
+    public float sofaMass;
+    public float tabureteMass;
+    public float taquillaMass;
 
     private float inputX;
     private float inputZ;
@@ -61,36 +90,104 @@ public class SimpleMasMov : MonoBehaviour
     {
         switch (prefab)
         {
-            case "Fridge":
-                mass = fridgeMass;
+            case "Armario":
+                mass = armarioMass;
+                break; 
+            case "Barril1":
+                mass = barril1Mass;
+                break;            
+            case "Barril2":
+                mass = barril2Mass;
                 break;
-            case "Botella":
+            case "Basura":
+                mass = basuraMass;
                 break;
-            case "Taburete":
+            case "Bombona":
+                mass = bombonaMass;
                 break;
-            case "Sofa":
+            case "Botella1":
+                mass = botella1Mass;
                 break;
-            case "Silla":
-                break;
-            case "Mesa":
-                break;
-            case "Maceta":
-                break;
-            case "Lata":
-                break;
-            case "Lampara":
-                break;
-            case "Jarra":
+            case "Botella2":
+                mass = botella2Mass;
                 break;
             case "Caja":
+                mass = cajaMass;
                 break;
+            case "Cama":
+                mass = camaMass;
+                break;            
+            case "Colchon":
+                mass = colchonMass;
+                break;            
+            case "Cono":
+                mass = conoMass;
+                break;
+            case "Estanteria":
+                mass = estanteriaMass;
+                break;
+            case "Jarra":
+                mass = jarraMass;
+                break;
+            case "Ladrillo":
+                mass = ladrilloMass;
+                break;            
+            case "LataArrugada":
+                mass = lataArrugadaMass;
+                break;            
+            case "LataEntera":
+                mass = lataEnteraMass;
+                break;            
+            case "Libro":
+                mass = libroMass;
+                break;
+            case "Lampara":
+                mass = lamparaMass;
+                break;
+            case "Maceta":
+                mass = macetaMass;
+                break;
+            case "Mesa":
+                mass = mesaMass;
+                break;
+            case "Mesilla":
+                mass = mesillaMass;
+                break;            
+            case "MiniArmario":
+                mass = miniArmarioMass;
+                break;           
+            case "Fridge":
+                mass = fridgeMass;
+                break;            
+            case "Noodles":
+                mass = noodlesMass;
+                break;           
+            case "Pale":
+                mass = paleMass;
+                break;            
+            case "Silla":
+                mass = sillaMass;
+                break;            
+            case "Sillon":
+                mass = sillonMass;
+                break;            
+            case "Sofa":
+                mass = sofaMass;
+                break;            
+            case "Taburete":
+                mass = tabureteMass;
+                break;            
+            case "Taquilla":
+                mass = taquillaMass;
+                break;
+
+
         }
         rbApplication();
     }
     private void rbApplication()
     {
         rb.mass = mass;
-        Debug.Log(rb.mass);
         rb.useGravity = true;
     }
     private void OnEnable()
