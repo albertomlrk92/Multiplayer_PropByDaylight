48fc1ed70df4e71b5b9b9dc5f4ea9eafdb97f285
El cremas <eduexile@gmail.com>
2020-06-15 05:27:06 +0200

    A tomar por cuilo

--- PropHunt/Assets/Script/Player/ChooseCharacter.cs
+++ PropHunt/Assets/Script/Player/ChooseCharacter.cs
@@ -6,6 +6,14 @@ public class ChooseCharacter : MonoBehaviour
 {
     public GameObject prop;
     public GameObject hunter;
+
+    public Camera scene;
+
+
+    [SerializeField]
+    Behaviour[] hunterComponents;
+    [SerializeField]
+    Behaviour[] propComponents;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,12 +26,41 @@ public class ChooseCharacter : MonoBehaviour
         {
             hunter.SetActive(true);
             hunter.transform.root.name = "Hunter";
+
+            
+
         }
+    }
+
+    void EnableComponentsHunter()
+    {
+        for (int i = 0; i < hunterComponents.Length; i++)
+        {
+            hunterComponents[i].enabled = true;
+        }
+    }
+
+    void EnableComponentsProp()
+    {
+        for (int i = 0; i < propComponents.Length; i++)
+        {
+            propComponents[i].enabled = true;
+        }
+    }
+    void SceneCameraDisable()
+    {
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(hunter.activeSelf)
+        {
+            EnableComponentsHunter();
+        }
+        if (prop.activeSelf)
+        {
+            EnableComponentsProp();
+        }
     }
 }
