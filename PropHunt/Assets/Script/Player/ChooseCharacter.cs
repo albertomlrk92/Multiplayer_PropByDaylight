c6470dece70f8c4bad6fc9eb540651c232049db1
Kendo777 <36260994+Kendo777@users.noreply.github.com>
2020-06-15 04:46:16 +0200

    prop

---
+++ PropHunt/Assets/Script/Player/ChooseCharacter.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseCharacter : MonoBehaviour
+{
+    public GameObject prop;
+    public GameObject hunter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(GameObject.Find("Hunter")!=null)
+        {
+            prop.SetActive(true);
+            prop.transform.root.name = "Prop";
+        }
+        else
+        {
+            hunter.SetActive(true);
+            hunter.transform.root.name = "Hunter";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
