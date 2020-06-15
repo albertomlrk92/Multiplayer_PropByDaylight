12f1b6347b8b18a48f17f45454d49acc7b68afb5
El cremas <eduexile@gmail.com>
2020-06-15 06:23:52 +0200

    Blabla

--- PropHunt/Assets/CustomNetwork.cs
+++ PropHunt/Assets/CustomNetwork.cs
@@ -5,26 +5,19 @@ using UnityEngine.Networking;
 
 public class CustomNetwork : NetworkManager
 {
-
-    List<GameObject> players = new List<GameObject>();
-
+    public GameObject propPrefab;
     public GameObject hunterPrefab;
     int count = 0;
 
     public override void OnServerAddPlayer(NetworkConnection conn, short playerControllerId)
     {
-        if(numPlayers > count)
+       if(numPlayers<2)
         {
             playerPrefab = hunterPrefab;
+        }else
+        {
+            playerPrefab = propPrefab;
         }
-       
-       
-        GameObject player = (GameObject)Instantiate(playerPrefab, Vector3.zero, Quaternion.identity);
-        players.Add(player);
-        Debug.Log("Se ha añadidio un player " + player + "count: " + count);
-        count++;
-        //player.GetComponent<Player>().color = Color.red;
-        NetworkServer.AddPlayerForConnection(conn, player, playerControllerId);
     }
 
 
